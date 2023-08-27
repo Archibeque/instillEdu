7371531e9b36e957eacceed09ce0a04a21cb024f
nnadidan360 <nnadidan360@gmail.com>
2023-08-27 09:42:15 +0100

    changed url to https

--- src/app_state/apiCalls.js
+++ src/app_state/apiCalls.js
@@ -9,7 +9,7 @@ import {
 export const getMovies = async (dispatch, updated) => {
     dispatch(getMovieStart());
     try {
-      const data = await fetch(`http://www.omdbapi.com/?i=${process.env.REACT_APP_ID}&apikey=${process.env.REACT_APP_APIKEY}&s=${updated}`)
+      const data = await fetch(`https://www.omdbapi.com/?i=${process.env.REACT_APP_ID}&apikey=${process.env.REACT_APP_APIKEY}&s=${updated}`)
     const response = await data.json()
     console.log(response)
       dispatch(getMovieSuccess(response.Search));
